a840d6e21a71bd58d40c90d11985a70c483fbb8f
asmodeus <allforyt96@gmil.com>
2021-04-19 18:43:31 +0530

    all done

--- Bigger but small projects/PriceWebScrapper.py
+++ Bigger but small projects/PriceWebScrapper.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time 
 
 URL="https://www.amazon.in/gp/product/B079S811J3/ref=ox_sc_act_image_1?smid=A14CZOWI0VEHLG&psc=1"
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 OPR/73.0.3856.427'}
+
 
 def price_check():
     page = requests.get(URL, headers=headers)
@@ -20,7 +22,8 @@ def price_check():
         price=price.replace(i,'')
 
     converted_price = int(price[1:5])
-    print(converted_price)
+    #print(converted_price)
+    return converted_price
 
 def email_sender():
     sender = 'botmail9000@gmail.com'
@@ -48,4 +51,16 @@ def email_sender():
     print("email sent")
     server.quit()
 
+def main():
+    print("Programm execution")
+    ek_aur_price=price_check()
 
+    if ek_aur_price<1799:
+        email_sender()
+    else:
+        print("price is still high")
+
+while True:
+    if __name__ == "__main__":
+        main()
+        time.sleep(3600)
